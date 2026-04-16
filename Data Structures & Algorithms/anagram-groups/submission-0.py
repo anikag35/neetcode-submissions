5f8269a7f04dc0977783c404d957f495dbf5e80b
Anika Giri <103085674+anikag35@users.noreply.github.com>
2026-04-15 17:47:16 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #create a check ifAnagrams function, and add that to a master list
+        myDict = {}
+        for string in strs:
+            if "".join(sorted(string)) in myDict:
+                myDict["".join(sorted(string))].append(string)
+            else:
+                myDict["".join(sorted(string))] = [string]
+        return list(myDict.values())
