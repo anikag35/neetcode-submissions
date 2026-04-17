a6930cf0447bf479a5124802329be0ed885d4207
Anika Giri <103085674+anikag35@users.noreply.github.com>
2026-04-17 12:41:13 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        currentMin = val if not self.min_stack else min(val, self.min_stack[-1])
+        self.min_stack.append(currentMin)
+
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
