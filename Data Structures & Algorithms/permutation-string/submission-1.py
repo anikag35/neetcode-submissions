5cb9195fd9df383c776d65af7b6f7fcd19c57f3e
Anika Giri <103085674+anikag35@users.noreply.github.com>
2026-04-17 01:06:45 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        windowSize = len(s1)
+
+        #counter defaults to 0 for keys that aren't in the dict yet
+        need = Counter(s1)
+        
+        #get the first valid window
+        curr = Counter(s2[:windowSize])
+         
+        if need == curr:
+            return True
+
+        for right in range(windowSize, len(s2)):
+            curr[s2[right]] += 1
+            leftChar = s2[right - windowSize]
+            curr[leftChar] -= 1
+            if (curr[leftChar] == 0):
+                del curr[leftChar]
+            if curr == need:
+                return True
+        return False
